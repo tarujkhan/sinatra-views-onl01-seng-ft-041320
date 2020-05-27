1dc2c268d2a51fe1bffc4308f41ee12caa01f24f
taroj khan <tarujkhan@yahoo.com>
2020-05-27 20:45:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ class App < Sinatra::Base
 # 	end
 	
 	get '/' do
+<<<<<<< HEAD
 	erb :index
 	end 
 	
@@ -14,3 +15,8 @@ class App < Sinatra::Base
       erb :info
     end
 end
+=======
+		"<h1>Hello World</h1>"
+	end
+end
+>>>>>>> c94cf6d58d1d220f5f166b7d0cdcce827fd5298c
